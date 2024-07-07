b3713f977988ba23def8ac62ff5233781392a617
faridmustafazade <mustafazadefaridm@gmail.com>
2024-07-07 18:44:09 +0400

    Updates: fix codes

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -14,96 +14,10 @@ const Sidebar = () => {
 
   return (
     <>
-      <aside className="h-screen fixed z-[999] left-0 flex flex-col">
-        <nav className="h-screen flex flex-col justify-between bg-[#eaebed] border-r shadow">
-          <div>
-            <div
-              className={`p-4 pb-2 flex  items-center ${
-                click ? "justify-between" : "justify-center"
-              }`}
-            >
-              <div
-                className={`
-           flex justify-between items-center
-           overflow-hidden transition-all ${
-             click ? "w-52 ml-3 opacity-100" : "w-0 opacity-0"
-           }`}
-              >
-                <img src={logo} className="w-52 max-w-52" alt="" />
-              </div>
-              <button
-                onClick={() => setClick(!click)}
-                className="p-1.5 rounded-lg"
-              >
-                {click ? (
-                  <BiMenuAltRight className="text-2xl" />
-                ) : (
-                  <IoMenu className="text-2xl" />
-                )}
-              </button>
-            </div>
-
-            <div className="mt-10 flex flex-col gap-3">
-              <Link
-                to={"/new_complaint"}
-                className="cursor-pointer flex p-3 bg-white mx-3 rounded-md"
-              >
-                <FiPlus className="w-5 h-5 rounded-md" />
-                <div
-                  className={`
-              flex justify-between items-center
-              overflow-hidden transition-all ${
-                click ? "w-52 ml-3 opacity-100" : "w-0 opacity-0"
-              }
-          `}
-                >
-                  <div className="leading-4">
-                    <h4 className="w-44 max-w-44">Yeni şikayət yarat</h4>
-                  </div>
-                </div>
-              </Link>
-              <Link
-                to={"/"}
-                className="cursor-pointer flex p-3 bg-white mx-3 rounded-md"
-              >
-                <TfiMenuAlt className="w-5 h-5 rounded-md" />
-                <div
-                  className={`
-              flex justify-between items-center
-              overflow-hidden transition-all ${
-                click ? "w-52 ml-3 opacity-100" : "w-0 opacity-0"
-              }
-          `}
-                >
-                  <div className="leading-4">
-                    <h4 className="w-44">Şikayətlər</h4>
-                  </div>
-                </div>
-              </Link>
-              <div
-                onClick={() => setChoose(true)}
-                className="cursor-pointer flex p-3 bg-white mx-3 rounded-md"
-              >
-                <img src={hand} alt="hand" className="w-5 h-5 rounded-md" />
-                <div
-                  className={`
-              flex justify-between items-center
-              overflow-hidden transition-all ${
-                click ? "w-52 ml-3 opacity-100" : "w-0 opacity-0"
-              }
-          `}
-                >
-                  <div className="leading-4">
-                    <h4 className="w-44 text-sm">
-                      İstehlakçı təcrübəsi sorğusu{" "}
-                    </h4>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+      <aside className="h-screen bg-[#eaebed] flex flex-col justify-between border-r shadow fixed z-[999] left-0">
+        <div>
           <div
-            className={`p-4 pb-2 flex  items-center bg-[#e2e3e4] shadow ${
+            className={`p-4 pb-2 flex  items-center ${
               click ? "justify-between" : "justify-center"
             }`}
           >
@@ -114,20 +28,101 @@ const Sidebar = () => {
              click ? "w-52 ml-3 opacity-100" : "w-0 opacity-0"
            }`}
             >
-              <p className="w-52 max-w-52 font-semibold" alt="">
-                Çıxış
-              </p>
+              <img src={logo} className="w-52 max-w-52" alt="" />
             </div>
             <button
               onClick={() => setClick(!click)}
               className="p-1.5 rounded-lg"
             >
-              <CiLogout
-                className={`text-2xl duration-500 ${click && "rotate-180"}`}
-              />
+              {click ? (
+                <BiMenuAltRight className="text-2xl" />
+              ) : (
+                <IoMenu className="text-2xl" />
+              )}
             </button>
           </div>
-        </nav>
+
+          <div className="mt-10 flex flex-col gap-3">
+            <Link
+              to={"/new_complaint"}
+              className="cursor-pointer flex p-3 bg-white mx-3 rounded-md"
+            >
+              <FiPlus className="w-5 h-5 rounded-md" />
+              <div
+                className={`
+              flex justify-between items-center
+              overflow-hidden transition-all ${
+                click ? "w-52 ml-3 opacity-100" : "w-0 opacity-0"
+              }
+          `}
+              >
+                <div className="leading-4">
+                  <h4 className="w-44 max-w-44">Yeni şikayət yarat</h4>
+                </div>
+              </div>
+            </Link>
+            <Link
+              to={"/"}
+              className="cursor-pointer flex p-3 bg-white mx-3 rounded-md"
+            >
+              <TfiMenuAlt className="w-5 h-5 rounded-md" />
+              <div
+                className={`
+              flex justify-between items-center
+              overflow-hidden transition-all ${
+                click ? "w-52 ml-3 opacity-100" : "w-0 opacity-0"
+              }
+          `}
+              >
+                <div className="leading-4">
+                  <h4 className="w-44">Şikayətlər</h4>
+                </div>
+              </div>
+            </Link>
+            <div
+              onClick={() => setChoose(true)}
+              className="cursor-pointer flex p-3 bg-white mx-3 rounded-md"
+            >
+              <img src={hand} alt="hand" className="w-5 h-5 rounded-md" />
+              <div
+                className={`
+              flex justify-between items-center
+              overflow-hidden transition-all ${
+                click ? "w-52 ml-3 opacity-100" : "w-0 opacity-0"
+              }
+          `}
+              >
+                <div className="leading-4">
+                  <h4 className="w-44 text-sm">
+                    İstehlakçı təcrübəsi sorğusu{" "}
+                  </h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          className={`p-4 pb-2 flex  items-center bg-[#e2e3e4] shadow ${
+            click ? "justify-between" : "justify-center"
+          }`}
+        >
+          <div
+            className={`
+           flex justify-between items-center
+           overflow-hidden transition-all ${
+             click ? "w-52 ml-3 opacity-100" : "w-0 opacity-0"
+           }`}
+          >
+            <p className="w-52 max-w-52 font-semibold" alt="">
+              Çıxış
+            </p>
+          </div>
+          <button onClick={() => setClick(!click)} className="p-1.5 rounded-lg">
+            <CiLogout
+              className={`text-2xl duration-500 ${click && "rotate-180"}`}
+            />
+          </button>
+        </div>
       </aside>
       <div
         className={`fixed inset-0 bg-black z-[9999] flex justify-center items-center ${
